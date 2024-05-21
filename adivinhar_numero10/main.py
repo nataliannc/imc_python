e29e626796a12158e1f04c7a0ad1d2a313fc9d99
natalianascimento <nataliannc@gmail.com>
2024-05-21 19:51:18 -0300

    imc_python

---
+++ adivinhar_numero10/main.py
@@ -0,0 +1,8 @@
+#adivinhe o número
+
+numero = int(input("Qual é o número que eu estou pensando?"))
+
+if numero == 5:
+    print("Você acertou. Parabéns!")
+else:
+    print("Você errou.")
---
+++ while11/main.py
@@ -0,0 +1,10 @@
+# Crie um progrma que o úsurio informa um número inteiro
+
+contador = int(input("Informe um número inteiro:"))
+
+#conta a partir do número informado até 0
+#-= -> subtrai a um valor e atribui o resultado
+
+while contador >= 0:
+    print(contador)
+    contador -= 1
