3bd692f87b2d4f0026b29922f4d0a0e938c7dff9
natalianascimento <nataliannc@gmail.com>
2024-05-21 21:06:28 -0300

    imc_python

--- while11/main.py
+++ while11/main.py
@@ -1,10 +1,18 @@
-# Crie um progrma que o úsurio informa um número inteiro
+#importa as bibliotecas
+import os
+import time
 
 contador = int(input("Informe um número inteiro:"))
+#permite acessar o promp de comando
+os.system('cls')
 
 #conta a partir do número informado até 0
 #-= -> subtrai a um valor e atribui o resultado
 
 while contador >= 0:
-    print(contador)
+    print(f'Contagem regressiva: {contador}')
+    time.sleep(100) #função da biblioteca time: mostra a contagem de 1 em 1 segundo
+    os.system('cls')
     contador -= 1
+
+print("BOOOM!!!")
