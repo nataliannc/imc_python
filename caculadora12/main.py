9044164351a51474c399ac1e3bec9932c2b0c697
natalianascimento <nataliannc@gmail.com>
2024-05-21 21:49:42 -0300

    v 1.0

---
+++ caculadora12/main.py
@@ -0,0 +1,36 @@
+"""
+calculadora feita em python
+que recebe dois números e calcula a operação desejada 
+pelo usúario
+"""
+while True: #loop infinito
+
+    n1 = str(input("Digite o primeiro número:")).replace(',','.')
+    n2 = str(input("Digite o segundo número:")).replace(',','.')
+
+    n1 = float(n1)
+    n2 = float(n2)
+
+    print('Operações:')
+    print('"/" divisão')
+    print('"+" soma')
+    print('"-"subtração')
+    print('"*" multiplicação')
+    print('"%" resto')
+
+    operacao = input(("Qual operação você deseja realizar?"))
+
+    match operacao:
+        case '+':
+            print(f"A some é: {n1 + n2}")
+        case '-':
+            print(f"A subtração é: {n1 - n2}")
+        case '*':
+            print(f"A multiplicação é: {n1 * n2}")
+        case '/':
+            print(f"A divisão é: {n1 / n2}")
+        case '%':
+            print(f"O resto da divisão é: {n1 % n2}")
+        case _:
+            print("Operação inválida")
+            continue #interrompe o loop na linha de comando e volta para o inicio do proximo loop
