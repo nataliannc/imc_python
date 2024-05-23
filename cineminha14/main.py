7167c7eda36f8774ea530cf778df6165c3d79be7
natalianascimento <nataliannc@gmail.com>
2024-05-23 20:23:12 -0300

    Versão 2.2

--- cineminha14/main.py
+++ cineminha14/main.py
@@ -1,37 +1,43 @@
-nome = input("Qual é o seu nome?")
+import os #traz os comamandos do sistema operacional para o python
+
+nome = str(input("Qual é o seu nome?"))
 idade = int(input("Qual é a sua idade?"))
+
+os.system("cls") #limpa console
 
 while True:
 
-    filme1 = "Sala 1 - Procurando o Nemo. Classifiação indicativa: Livre"
-    filme2 = "Sala 2 - Pikachu. Classifiação indicativa: Livre"
-    filme3 = "Sala 3 - Senhor dos Aneis. Classifiação indicativa: Livre"
-    filme4 = "Sala 4 - Star Wars. Classifiação indicativa: +10 anos"
-    filme5 = "Sala 5 - O Pianista. Classifiação indicativa: +12 anos"
+    filme1 = "Sala 1 - Procurando o Nemo. Classificação indicativa: Livre"
+    filme2 = "Sala 2 - Pikachu. Classificação indicativa: Livre"
+    filme3 = "Sala 3 - Senhor dos Aneis. Classificação indicativa: Livre"
+    filme4 = "Sala 4 - Star Wars. Classificação indicativa: +10 anos"
+    filme5 = "Sala 5 - O Pianista. Classificação indicativa: +12 anos"
 
-    print("LISTA DE FILMES\n")
+    print(f"{"="*20} LISTA DE FILMES {"="*20}\n") #{"="*20} -> mostra o sinal de = 20 vezes seguidas
     print(filme1)
     print(filme2)
     print(filme3)
     print(filme4)
     print(filme5)
 
-    sala = int(input("informe a sala desejada:"))
+    sala = int(input("Informe a sala desejada:"))
+
+    os.system("cls")
 
     if sala == 1 and idade >=0:
-        print(f"Filme escolhido por {nome}: {filme1} Divirta-se!")
+        print(f"Filme escolhido por {nome}:\n {filme1}. Divirta-se!")
     elif sala == 2 and idade >=0:
-        print(f"Filme escolhido por {nome}: {filme2} Divirta-se!")
+        print(f"Filme escolhido por {nome}:\n {filme2}. Divirta-se!")
     elif sala == 3 and idade >=0:
-        print(f"Filme escolhido por {nome}: {filme3} Divirta-se!")
+        print(f"Filme escolhido por {nome}:\n {filme3}. Divirta-se!")
     elif sala == 4 and idade >=10:
-        print(f"Filme escolhido por {nome}: {filme4} Divirta-se!")
+        print(f"Filme escolhido por {nome}:\n {filme4}. Divirta-se!")
     elif sala == 5 and idade >=12:
-        print(f"Filme escolhido por {nome}: {filme5} Divirta-se!")
+        print(f"Filme escolhido por {nome}:\n {filme5}. Divirta-se!")
     else:
-        print("Você não pode assistir este filme. Escolha outro filme.")
+        print("Você não pode assistir este filme.")
 
-    continuar = input("Deseja escolher outro filme (s/n)?")
+    continuar = input("Deseja escolher um filme apropriado para a sua idade (s/n)?")
 
     if continuar == "s":
         continue
---
+++ lista 16/main.py
@@ -0,0 +1,39 @@
+#lista
+
+nomes = ["Alex", "Simone", "Bernardo", "César", "Alexandra"]
+
+"Primeira instrução"
+#exibe a lista na tela
+print(nomes)
+
+#exime o primeiro elemento da lista
+print(nomes[0]) 
+
+#foi declarada a variavel 'nome' para os elementos da lista 'nomes'. 
+for nome in nomes:
+    print(nome)
+
+#range = função para informar quantas vezes o laço for vai ser executado
+for i in range(3): #foi declarada a variavel i, 3 exibe os 3 primeiros elementos da lista
+    print(nomes[i])
+
+#quando não se sabe quantos elemento se tem na lista
+#len é uma função para medir o tamanho da variavel quando não se sabe o tamanho dela
+for i in range(len(nomes)):
+    print(nomes[i])
+
+
+for i in range(len(nomes)):
+    print(f'{i + 1}º nome da lista: {nomes[i]}.')
+
+#ordana a lista em ordem alfabetica / sort = organizar
+nomes.sort()
+
+for nome in nomes:
+    print(nome)
+
+#ordana a lista em ordem reversa
+nomes.sort(reverse=True)
+
+for nome in nomes:
+    print(nome)
