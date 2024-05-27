397fd247ff7c9c673c9ef31804948503bf76a76f
Natália do Nascimento <nataliannc@gmail.com>
2024-05-26 22:57:11 -0300

    v4

--- crud_de_nomes22/main.py
+++ crud_de_nomes22/main.py
@@ -12,16 +12,68 @@ crie um programa que tenha as opções:
 
 lista_de_pessoas = []
 
-nome = input("Insira o nome:")
+while True:
 
-lista_de_pessoas.append(nome)
+    print(f"{'='*20} AÇÕES DISPONÍVEIS {'='*20}\n")
+    print("Cadastrar (inserir) pessoa na lista: DIGITE 1")
+    print("Listar pessoas cadastradas: DIGITE 2")
+    print("Pesquisar o nome de uma pessoa: DIGITE 3")
+    print("Ordenar a lista por ordem alfabética: DIGITE 4")
+    print("Atualizar o nome de uma pessoa: DIGITE 5")
+    print("Deletar o nome de uma pessoa: DIGITE 6")
+    print("Sair do programa: DIGITE 7")
+    print(f"{'='*60}")
 
-for pessoa in lista_de_pessoas:
-    print(pessoa)
+    opcao_usuario = input("Informe a opção desejada:")
 
-print("O que você deseja fazer?")
-print("Deletar o nome de uma pessoa: digite 1")
-print("Pesquisar o nome de uma pessoa: digite 2")
-print("Ordenar a lista por ordem alfabetica: digite 3")
-print("Atualizar o nome de uma pessoa: digite 4")
-print("Sair do programa: digite 5")
+    #cadastrar nome
+    if opcao_usuario == "1":
+        nome = input("Informe o nome da pessoa:")
+        lista_de_pessoas.append(nome)
+        print("Nome cadastrado!")
+
+    #listar nomes da lista
+    elif opcao_usuario == "2":
+        print(f"Nomes da lista: {lista_de_pessoas}")
+
+    #pesquisar nomes na lista
+    elif opcao_usuario == "3":
+        nome = input("Informe o nome da pessoa a ser pesquisada:")
+        if nome in lista_de_pessoas:
+            print("Nome encontrado na lista.")
+        else:
+            print("Nome não encontrado na lista.")
+
+    #ordenar a lista por odem alfabética
+    elif opcao_usuario == "4":
+        lista_de_pessoas.sort()
+        print(f"Lista em ordem alfabética:{lista_de_pessoas}")
+
+    #atualizar nomes da lista
+    elif opcao_usuario == "5":
+        nome_para_atualizar = input("Informe o nome que deseja alterar:")
+        if nome_para_atualizar in lista_de_pessoas:
+            nome_atualizado = input("Informe o novo nome:")
+            index = lista_de_pessoas.index(nome_para_atualizar)
+            lista_de_pessoas[index] = nome_atualizado
+            print("Nome atualizado.")
+        else:
+            print("Nome não encontrado.")
+
+    #deletar nomes da lista
+    elif opcao_usuario == "6":
+        nome = input("informe o nome da pessoa a ser deletado:")
+        if nome in lista_de_pessoas:
+            lista_de_pessoas.remove(nome)
+            print("Nome deletado.")
+        else:
+            print("Nome não encontrado.")
+
+    #sair do programa
+    elif opcao_usuario == "7":
+        print("Você saiu do programa. Volte sempre!")
+        break
+
+    #opção inválida
+    else:
+        print("Opção inválida.")
