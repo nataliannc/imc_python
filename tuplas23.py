38f1a96cfc5b7d2a9e155cabc7687bed25c8d44c
Natália do Nascimento <nataliannc@gmail.com>
2024-05-28 21:15:29 -0300

    v1

---
+++ tuplas23.py
@@ -0,0 +1,16 @@
+#tupla
+
+meses = ("Janeiro", "Fevereiro", "Março", "Abril", "Maio")
+
+#imprime a tupla
+for mes in meses:
+    print(mes)
+
+#tupla
+chaves = ("Nome", "Idade", "Profissão")
+
+pessoa = {
+    chaves[0] : "Alex",
+    chaves[1]: 39,
+    chaves[2]: "Programador"
+}
