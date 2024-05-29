c3d21eb644be8f9ff6457cd13ddbc4d91e84b932
natalianascimento <nataliannc@gmail.com>
2024-05-29 19:58:11 -0300

    v1

---
+++ lista_dicionario28/main.py
@@ -0,0 +1,29 @@
+#tupla
+chaves = ("Nome", "Idade", "E-mail", "Profissão")
+
+#lista de dicionários
+pessoas = [
+    {
+        chaves[0]: "Alex Machado",
+        chaves[1]: 39,
+        chaves[2]: "alex@gmail.com",
+        chaves[3]: "Programador"
+    },
+    {
+        chaves[0]: "Ana Julia",
+        chaves[1]: 50,
+        chaves[2]: "ana@gmail.com",
+        chaves[3]: "Dentista"
+    },
+    {
+        chaves[0]: "Pablo",
+        chaves[1]: 18,
+        chaves[2]: "escobar@gmail.com",
+        chaves[3]: "Vendedor"
+    }
+]
+
+#exibe a lista
+for pessoa in pessoas:
+    for chave in pessoa:
+        print(f"{chave}:{pessoa.get(chave)}")
