368f48565ef45e395374fb581360371688587899
natalianascimento <nataliannc@gmail.com>
2024-06-03 21:28:43 -0300

    v2

--- atividade_funcao33/main.py
+++ atividade_funcao33/main.py
@@ -2,6 +2,8 @@
 Crie um programa que o usuário possa escolher se deseja saber a área de um círculo, de um triângulo ou de um trapézio.
 """
 
+import os
+
 def exibir_menu():
     print("Digite 1 para calcular a área de um círculo.")
     print("Digite 2 para calcular a área de um triângulo.")
@@ -12,8 +14,8 @@ def calcular_circulo(raio):
     circulo = (raio**2) * 3.14
     return circulo
 
-def calcular_triangulo(base_circulo, altura_triangulo):
-    triangulo = (base_circulo * altura_triangulo)/2
+def calcular_triangulo(base_triangulo, altura_triangulo):
+    triangulo = (base_triangulo * altura_triangulo)/2
     return triangulo
 
 def calcular_trapezio(base1, base2, altura_trapezio):
@@ -25,13 +27,15 @@ while True:
     exibir_menu()
     opcao = input("Qual área você deseja calcular:")
 
+    os.system('cls')
+
     if opcao == "1":
         raio = int(input('Informe a base do cículo:'))
         print(f'A área do círculo é {calcular_circulo(raio)}')
     elif opcao == "2":
-        base_circulo = int(input("Informe o valor da base do triângulo:"))
+        base_triangulo = int(input("Informe o valor da base do triângulo:"))
         altura_triangulo = int(input("Informe o valor da altura do triângulo:"))
-        print(f'A área do triângulo é {calcular_triangulo(base_circulo, altura_triangulo)}')
+        print(f'A área do triângulo é {calcular_triangulo(base_triangulo, altura_triangulo)}')
     elif opcao == "3":
         base1 = int(input("Informe o valor da primeira base do trapézio:"))
         base2 = int(input("Informe o valor da segunda base do trapézio:"))
