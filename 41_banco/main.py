f6ca642ea0a9acebde79080b180d53b7cd99e61c
natalia <nataliannc@gmail.com>
2024-06-06 21:42:53 -0300

    Versão 1.0

--- 41_banco/main.py
+++ 41_banco/main.py
@@ -1,4 +1,8 @@
 """
+INCOMPLETO
+"""
+
+"""
 Crie um aplicativo de banco, onde o usuário possa:
 
 - Cadastrar seu nome no aplicativo.
@@ -15,3 +19,36 @@ Ao entrar no aplicativo, o usuário pode:
 Ao terminar, envie para o GitHub e poste o link.
 """
 
+campos = ("Nome")
+pessoas = []
+
+while True:
+
+    print(f"{'*'*20} AÇÕES DISPONÍVEIS {'*'*20}\n")
+    print("Cadastrar nome: DIGITE 1")
+    print("Deletar nome: DIGITE 2")
+    print("Entrar no aplicativo: DIGITE 3")
+    print(f"\n{'*'*59}")
+
+    opcao_usuario = input("Informe a opção desejada:")
+
+    #cadastrar pessoa
+    if opcao_usuario == "1":
+        print("Informe os dados abaixo:")
+        nome = input("Informe o nome:")
+        pessoa = (nome)
+        pessoas.append(pessoa)
+        print("Pessoa cadastrada!")
+
+    #deletar nomes da lista
+    elif opcao_usuario == "2":
+        pessoa_deletada = input("Informe o nome da pessoa a ser deletado:")
+        try:
+            pessoas.remove(pessoa_deletada)
+            print("Nome deletado!")
+        except:
+            print("Não foi possível deletar o nome informado. Informe um nome existente.")
+
+    #entrar no aplicativo
+    elif opcao_usuario == "3":
+        
