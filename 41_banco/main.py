ecd798cdfa6f1820aff70e59f64976d78bea87dd
natalia <nataliannc@gmail.com>
2024-06-06 21:21:11 -0300

    versão 1.0

---
+++ 41_banco/main.py
@@ -0,0 +1,17 @@
+"""
+Crie um aplicativo de banco, onde o usuário possa:
+
+- Cadastrar seu nome no aplicativo.
+- Deletar seu nome do aplicativo.
+- Entrar no aplicativo.
+
+Ao entrar no aplicativo, o usuário pode:
+
+- Consultar seu saldo (criar uma nova conta o saldo começa com R$ 0,00; a consulta deverá exibir a data da consulta)
+- Depositar valor.
+- Sacar valor.
+- Sair do programa.
+
+Ao terminar, envie para o GitHub e poste o link.
+"""
+
